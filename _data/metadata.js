ca6963e0b7f7cc96e0b2befe215b0511ac63e6c7
Yahya J. Aifit <yahya.junianto@bukalapak.com>
2023-12-28 08:37:47 +0700

    checkpoint navbar

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,5 +1,6 @@
 module.exports = {
 	title: "Eleventy Base Blog v8",
+	logoText: "Ar.",
 	url: "https://example.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
